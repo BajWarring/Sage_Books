e3dd241c6b279cfab2e5cb593a70352fbe50dd34
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-01-10 13:22:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sage_books/login_page.dart';
-import 'package:sage_books/home_page.dart'; // Import HomePage
+import 'package:sage_books/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,21 +21,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // Add your theme data here if needed
       ),
-      // --- AUTH PERSISTENCE LOGIC ---
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          // 1. Loading State
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(body: Center(child: CircularProgressIndicator()));
           }
-
-          // 2. User is Logged In -> Go to Home
           if (snapshot.hasData) {
             return HomePage(user: snapshot.data!);
           }
-
-          // 3. User is Logged Out -> Go to Login
           return const LoginPage();
         },
       ),
